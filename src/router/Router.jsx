96e1c76f789bc24bf166088513ef6957d8fbd3c2
mahmoud <mahmmoudmagdy52@gmail.com>
2025-02-20 14:46:29 +0200

    setup router

---
+++ src/router/Router.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router'
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+        <Routes>
+            
+        </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Router
